e710edd0f08f001148894d88f265eb8a17b84f00
NabilRaiyan <rraiyan77@gmail.com>
2024-10-06 17:22:09 +0600

    creating floating Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "@react-icons/all-files/fa/FaHome";
+
 
 
 // Creating Home page
@@ -7,8 +11,15 @@ export default function Home() {
     <main className="relative bg-black-100 text-white flex justify-center 
                       items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          {
+            name: 'Home',
+            link: '/',
+            icon: <FaHome />
+          }
+        ]}/>
         <Hero />
-
+        <Grid />
       </div>
     </main>
   );
