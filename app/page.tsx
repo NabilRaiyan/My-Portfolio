b6c91c27c7cbff5f643a1feb33af12e65c212cff
NabilRaiyan <rraiyan77@gmail.com>
2024-10-06 16:38:53 +0600

    creating grid bg and adding spotlight in hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-
+import Hero from "@/components/Hero";
 
 
 // Creating Home page
@@ -7,7 +7,8 @@ export default function Home() {
     <main className="relative bg-black-100 text-white flex justify-center 
                       items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>Hello Portfolio</h1>
+        <Hero />
+
       </div>
     </main>
   );
