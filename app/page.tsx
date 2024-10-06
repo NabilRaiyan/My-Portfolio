76152d755eebc88ce2acad85f55c8b929f818a0c
NabilRaiyan <rraiyan77@gmail.com>
2024-10-06 19:54:44 +0600

    creating github globe

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "@react-icons/all-files/fa/FaHome";
-
+import { navItems } from "@/data";
 
 
 // Creating Home page
@@ -11,13 +10,7 @@ export default function Home() {
     <main className="relative bg-black-100 text-white flex justify-center 
                       items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {
-            name: 'Home',
-            link: '/',
-            icon: <FaHome />
-          }
-        ]}/>
+        <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
       </div>
